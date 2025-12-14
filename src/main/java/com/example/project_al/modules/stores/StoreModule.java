28fd1e1bb436ef25d8296ba9d207d886ff7b626d
benyou10 <ybenzian016@gmail.com>
2025-12-14 14:02:18 +0100

    third push

--- src/main/java/com/example/project_al/modules/stores/StoreModule.java
+++ src/main/java/com/example/project_al/modules/stores/StoreModule.java
@@ -1,6 +1,5 @@
 package com.example.project_al.modules.stores;
 
 public interface StoreModule {
-    // Just an interface to mark the module
-    // Spring Modulith will detect it automatically
+
 }
